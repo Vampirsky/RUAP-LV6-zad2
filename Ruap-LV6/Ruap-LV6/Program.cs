742087d38fe4563186c065b1c46d1a992f2b3cf1
Kristijan <birtic.kristijan@gmail.com>
2016-05-24 17:40:02 +0200

    initial commit

---
+++ Ruap-LV6/Ruap-LV6/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ruap_LV6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
